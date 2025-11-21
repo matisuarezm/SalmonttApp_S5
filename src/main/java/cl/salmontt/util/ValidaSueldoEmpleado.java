960327e81c5c2be0a9a5d5ad825ca9c54f19e874
matisuarezm <mati.suarez@duocuc.cl>
2025-11-21 17:47:10 -0300

    Actualiza validacion de sueldo

--- src/main/java/cl/salmontt/util/ValidaSueldoEmpleado.java
+++ src/main/java/cl/salmontt/util/ValidaSueldoEmpleado.java
@@ -1,5 +1,7 @@
 package cl.salmontt.util;
 
+import java.util.IllegalFormatCodePointException;
+
 public class ValidaSueldoEmpleado {
 
     /**
@@ -8,8 +10,20 @@ public class ValidaSueldoEmpleado {
      * @throws IllegalArgumentException si el sueldo es negativo
      */
     public static void validar(double sueldo){
-        if (sueldo < 0){
+
+        final double SUELDO_MINIMO = 529000;
+        final double SUELDO_MAXIMO = 10000000;
+
+        if (sueldo <= 0){
             throw new IllegalArgumentException("El sueldo no puede ser menor a 0.");
+        }
+
+        if (sueldo < SUELDO_MINIMO){
+            throw new IllegalArgumentException("El sueldo no puede ser menor al mínimo legal");
+        }
+
+        if (sueldo > SUELDO_MINIMO){
+            throw new IllegalArgumentException("El sueldo es demasiado alto, verifica la información");
         }
     }
 }
